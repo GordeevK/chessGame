97db021e468437a44945ee469cd6025e37d60151
GordeevK <Gordeevka05@mail.ru>
2025-02-22 09:45:08 +0500

    change

--- board.py
+++ board.py
@@ -1,5 +1,26 @@
-class board():
+class Board:
     def __init__(self):
-        pass
-    def printBoard(self):
-        print("Это доска")
+        self.__board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+        ]
+    def print_board(self) -> None:
+        print("   1  2  3  4  5  6  7  8")
+        for id_line in range(8):
+            print((8 - id_line), "|", end="")
+            for square in self.__board[id_line]:
+                if not square:
+                    print("  |", end="")
+                else:
+                    print(square + "|", end="")
+            print("")
+        print("   1  2  3  4  5  6  7  8")
+
+a = Board()
+a.print_board()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from board import board
+from board import Board
 
-a = board()
+a = Board()
 a.printBoard()
