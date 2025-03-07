cf0dcddb0ce7ff3fc11cdcee71f31bc487d3bef1
GordeevK <165748078+GordeevK@users.noreply.github.com>
2025-03-07 08:29:34 +0500

    Update king.py

--- pieces/king.py
+++ pieces/king.py
@@ -2,4 +2,20 @@ from piece import Piece
 
 
 class King(Piece):
-  pass
+   def __str__(self) -> str:
+        return self.color[0].lower() + "K"
+
+    def get_possible_moves(self, board: list[list]) -> list[tuple]:
+        moves = []
+        move_from = self.get_pos()
+        directions = [(0, -1), (0, 1), (1, 0), (-1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+        for dx, dy in directions:
+            x, y = move_from
+            if 0 <= x + dx < 8 and 0 <= y + dy < 8:
+                x += dx
+                y += dy
+                if self.can_move(move_from, (x, y), board):
+                    moves.append((x, y))
+                if board[x][y] is not None:
+                    break
+        return moves
